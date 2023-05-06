286c1776e1bb75c3d6f1ed26242f4cf4a1cf6242
Paweł Bojnowski <pawel.bojnowski@gmail.com>
2023-05-06 22:37:59 +0200

    First commit

---
+++ infrastructureSetup/runKafkaAvro.sh
@@ -0,0 +1,184 @@
+#!/bin/bash
+
+
+#remove existing docker images
+ids=$(docker ps -aqf name=kce_.)
+for id in $(echo $ids | tr "\n" " "); do
+  docker stop  $id
+  docker container rm -f $id
+done
+
+#remove existing docker volumes
+rm -rf ./volumes
+
+#create docker
+docker-compose -f docker-compose-avro.yml up --build -d --remove-orphans
+
+
+while [[ $(curl -s -H "Content-Type: application/json" -XGET 'http://localhost:8083/connectors') != "[]" ]];
+do
+  printf "."
+  sleep 1
+done
+sleep 3
+echo "\n----------------------------------------------------"
+echo "Create connectors:\n"
+
+curl -s -H "Content-Type: application/json" -XPOST 'http://localhost:8083/connectors' -d '{
+   "name":"postgres.connector.source",
+   "config":{
+      "topic.prefix":"postgres.connector.source.",
+      "connector.class":"io.confluent.connect.jdbc.JdbcSourceConnector",
+      "tasks.max":"1",
+      "connection.url":"jdbc:postgresql://kce_postgres:5432/postgres",
+      "connection.user":"postgres",
+      "connection.password":"postgres",
+      "mode":"incrementing",
+      "table.whitelist" : "user",
+      "incrementing.column.name":"id",
+      "validate.non.null": "false",
+      "value.converter" : "class io.confluent.connect.avro.AvroConverter",
+      "transforms":"SetSchemaName",
+      "transforms.SetSchemaName.type":"org.apache.kafka.connect.transforms.SetSchemaMetadata$Value",
+      "transforms.SetSchemaName.schema.name":"pl.pb.kafkamodel.avro.User"
+
+   }
+}
+'  | json_pp
+
+
+curl -s -H "Content-Type: application/json" -XPOST 'http://localhost:8083/connectors' -d '{
+            "name": "postgres.connector.source.user.query.timestamp",
+            "config": {
+              "topic.prefix": "postgres.connector.source.user.query.timestamp",
+              "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
+              "tasks.max": "1",
+              "connection.url": "jdbc:postgresql://kce_postgres:5432/postgres",
+              "connection.user": "postgres",
+              "connection.password": "postgres",
+              "mode": "timestamp",
+              "query": "SELECT u.* FROM public.user u WHERE u.id > 1",
+              "validate.non.null": "false",
+
+              "transforms":"SetSchemaName",
+              "transforms.SetSchemaName.type":"org.apache.kafka.connect.transforms.SetSchemaMetadata$Value",
+              "transforms.SetSchemaName.schema.name":"pl.pb.kafkamodel.avro.User"
+            }
+          }
+'  | json_pp
+
+curl -s -H "Content-Type: application/json" -XPOST 'http://localhost:8083/connectors' -d '{
+            "name": "postgres.connector.source.user.query.incrementing",
+            "config": {
+              "topic.prefix": "postgres.connector.source.user.query.incrementing",
+              "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
+              "tasks.max": "1",
+              "connection.url": "jdbc:postgresql://kce_postgres:5432/postgres",
+              "connection.user": "postgres",
+              "connection.password": "postgres",
+              "mode": "incrementing",
+              "query": "Select t.* From (SELECT u.* FROM public.user u WHERE u.id > 1) t",
+              "incrementing.column.name": "id",
+              "validate.non.null": "false",
+
+              "transforms":"SetSchemaName",
+              "transforms.SetSchemaName.type":"org.apache.kafka.connect.transforms.SetSchemaMetadata$Value",
+              "transforms.SetSchemaName.schema.name":"pl.pb.kafkamodel.avro.User"
+            }
+          }
+'  | json_pp
+
+curl -s -H "Content-Type: application/json" -XPOST 'http://localhost:8083/connectors' \
+-d '{
+      "name": "postgres.connector.sink.user.insert",
+      "config": {
+        "topics": "postgres.connector.sink.user.insert",
+        "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
+        "tasks.max": "1",
+        "connection.url": "jdbc:postgresql://kce_postgres:5432/postgres",
+        "connection.user": "postgres",
+        "connection.password": "postgres",
+        "connection.ds.pool.size": 5,
+        "insert.mode.databaselevel": true,
+        "table.name.format": "user",
+        "auto.create": "false",
+        "auto.evolve": "true",
+        "insert.mode": "insert",
+        "delete.enabled": "false",
+        "schemas.enable": "false",
+        "key.converter.schemas.enable": "false",
+        "value.converter.schemas.enable": "true",
+        "fields.whitelist":"id,firstname,lastname,phone_number"
+      }
+    }'  | json_pp
+
+curl -s -H "Content-Type: application/json" -XPOST 'http://localhost:8083/connectors' \
+-d '{
+      "name": "postgres.connector.sink.client",
+      "config": {
+        "topics": "postgres.connector.sink.client",
+        "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
+        "tasks.max": "1",
+        "connection.url": "jdbc:postgresql://kce_postgres:5432/postgres",
+        "connection.user": "postgres",
+        "connection.password": "postgres",
+        "connection.ds.pool.size": 5,
+        "insert.mode.databaselevel": true,
+        "table.name.format": "client",
+        "auto.create": "false",
+        "auto.evolve": "true",
+        "insert.mode": "insert",
+        "delete.enabled": "false",
+        "schemas.enable": "false",
+        "key.converter.schemas.enable": "false",
+        "value.converter.schemas.enable": "true",
+        "fields.whitelist":"id,firstname,lastname,phone_number"
+      }
+    }'  | json_pp
+
+curl -s -H "Content-Type: application/json" -XPOST 'http://localhost:8083/connectors' \
+-d '{
+      "name": "postgres.connector.sink.user.update",
+      "config": {
+        "topics": "postgres.connector.sink.user.update",
+        "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
+        "tasks.max": "1",
+        "connection.url": "jdbc:postgresql://kce_postgres:5432/postgres",
+        "connection.user": "postgres",
+        "connection.password": "postgres",
+        "connection.ds.pool.size": 5,
+        "insert.mode.databaselevel": true,
+        "table.name.format": "user",
+        "auto.create": "false",
+        "auto.evolve": "true",
+        "insert.mode": "upsert",
+        "delete.enabled": "false",
+        "schemas.enable": "false",
+        "insert.mode": "upsert",
+        "delete.enabled": "false",
+        "pk.fields":"id",
+        "pk.mode": "record_value",
+        "key.converter.schemas.enable": "false",
+        "value.converter.schemas.enable": "true",
+        "fields.whitelist":"id,firstname,lastname,phone_number"
+      }
+    }'  | json_pp
+
+sleep 2
+
+echo "\n----------------------------------------------------"
+echo "List connectors:\n"
+
+curl -s -H "Content-Type: application/json" -XGET 'http://localhost:8083/connectors'  | json_pp
+
+echo "\n----------------------------------------------------"
+echo "List topic:\n"
+curl -s -H "Content-Type: application/vnd.kafka.v2+json" -XGET 'http://localhost:8082/topics' | json_pp
+
+
+
+
+
+
+
+
